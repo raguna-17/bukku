c04fd5131b95d61b7578ba940861ab1f8156633e
らぐな <pasupoto17@gmail.com>
2026-02-27 18:46:54 +0900

    Initial commit

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -17,7 +17,7 @@ from .models import User
 # =====================
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"),60)
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES",60))
 
 # =====================
 # Password Hashing
